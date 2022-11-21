c4f355bfc4f35307d8dd5acf808d4bb752ad9c7c
Bill Peterman <peterman.73@osu.edu>
2022-11-21 14:37:48 -0500

    0.1.1 – Added d_mat function

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# landconn 0.1.1
+
+* Added function to calculate pairwise distance matrices
+
+
 # landconn 0.1.0
 
 * Added a `NEWS.md` file to track changes to the package.
---
+++ R/dist_mat.R
@@ -0,0 +1,34 @@
+# Distance matrix -----------------------------------------
+
+#' Calculate pairwise distance matrix
+#'
+#' @param coords Provide a two-column array, matrix, or data.frame with xy coordinates of locations. Alternatively, an object of class `sf`.
+#' @return A square distance matrix
+#'
+#' @examples
+#' set.seed(123)
+#'
+#' ## Create random coordinates
+#' loc <- data.frame(x = runif(10,0,10),
+#'                   y = runif(10,0,10))
+#'
+#' ## Create distance matrix
+#' dist_mat <- d_mat(coords = loc)
+#'
+#' @usage d_mat(coords)
+#'
+#' @export
+#' @author Bill Peterman <peterman.73@@osu.edu>
+
+d_mat <- function(coords) {
+  if(any(class(coords) == "sf")){
+    d <- st_distance(coords)
+  } else {
+    if(dim(coords)[2] != 2){
+      stop("`coords` must be two columns representing xy coordinates")
+    }
+    d <- as.matrix(dist(coords))
+  }
+
+  return(d)
+}
